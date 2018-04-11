587014669c62769ee3af534f5f2c742a07776d1d
John Snow <thelunaticscripter@outlook.com>
2018-04-11 10:56:05 -0400

    Add start of Habitat plan
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

---
+++ habitat/plan.sh
@@ -0,0 +1,10 @@
+pkg_name=packer-resource
+pkg_origin=thelunaticscripter
+pkg_version="0.1.0"
+pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
+pkg_license=("Apache-2.0")
+pkg_deps=( core/packer )
+pkg_bin_dirs=(bin)
+pkg_description="Packer resource for Concourse CI."
+pkg_upstream_url="https://github.com/TheLunaticScripter/packer-resource"
+
