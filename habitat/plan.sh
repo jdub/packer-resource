c592a44a928be2b1fbe618aaec1dbdc7f1ae30b6
Aeron Baker <abaker9@gmail.com>
2018-04-11 09:43:32 -0700

    added install steps
    Signed-off-by: Aeron Baker <abaker9@gmail.com>

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -1,6 +1,6 @@
 pkg_name=packer-resource
 pkg_origin=thelunaticscripter
-pkg_version="0.1.0"
+pkg_version="0.1.1"
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
 pkg_license=("Apache-2.0")
 pkg_deps=( core/packer )
@@ -8,3 +8,15 @@ pkg_bin_dirs=(bin)
 pkg_description="Packer resource for Concourse CI."
 pkg_upstream_url="https://github.com/TheLunaticScripter/packer-resource"
 
+
+do_download() {
+ return 0
+}
+
+do_build() {
+  return 0
+}
+
+do_install() {
+ cp /src/bin -r /opt/resource
+}
